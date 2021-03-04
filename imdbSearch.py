a8876e9de9681605ade1432ca73d834c66ffc8e9
Giorgos Kosta <sc19gk@leeds.ac.uk>
2021-03-04 13:47:40 +0000

    added getUpcomingMovies function that returns an array of dictionaries like getMovieInfo of upcoming movies

--- imdbSearch.py
+++ imdbSearch.py
@@ -96,3 +96,65 @@ def getMovieInfo(movieTitle: str):
     return None
 
 
+
+def getUpcomingMovies():
+
+    apiKey="bd3bccc81b8b90568f58a7d8d3299477"
+    TMDBurl = "https://api.themoviedb.org/3/movie/upcoming?api_key={api_key}&language=en-US&page=1".format(api_key=apiKey)
+
+    TMDBresponse = requests.get(TMDBurl)
+    TMDBjson_response = TMDBresponse.json()
+
+    returnDictionary = []
+
+
+    # check for errors in the request and continue with the first movie from the search
+    if TMDBresponse.status_code == 200:
+        if not TMDBjson_response['total_results']>0:
+            return None
+        else:
+            for movie in TMDBjson_response['results']:
+                movieTitle = movie['original_title']
+                movieRating = movie['vote_average']
+                movieDescription = movie['overview']
+                moviePosterURL = "https://image.tmdb.org/t/p/w500{posterUrl}".format(posterUrl = movie['poster_path'])
+                tmdbID=movie['id']
+                movieTMDB_URL = "https://www.themoviedb.org/movie/{movie_id}".format(movie_id=tmdbID)
+
+
+                # get age rating
+                age_ratingURL = "https://api.themoviedb.org/3/movie/{movie_id}/release_dates?api_key={api_key}".format(movie_id = tmdbID, api_key=apiKey)
+                age_ratingResponse = requests.get(age_ratingURL)
+                age_ratingJsonResponse = age_ratingResponse.json()
+                age_rating = "Not Available"
+
+
+
+                # search for US data since it is the most likely to have this info available, many other regions in the response I looked at are empty
+                for country in age_ratingJsonResponse['results']:
+                    if country["iso_3166_1"] == "US":
+                        if country["release_dates"][0]["certification"] != '':
+                            age_rating = country["release_dates"][0]["certification"]
+
+                
+
+
+                # get duration
+                durationURL = "https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}".format(movie_id = tmdbID, api_key=apiKey)
+                durationResponse = requests.get(durationURL)
+                durationJsonResponse = durationResponse.json()
+                movieDuration = durationJsonResponse['runtime']
+                returnDictionary.append({"Title": movieTitle, "Rating": movieRating, "Description": movieDescription, "PosterURL": moviePosterURL, "InfoURL": movieTMDB_URL, "Age_Rating": age_rating, "Duration": movieDuration})
+
+
+
+
+    elif TMDBresponse.status_code == 404:
+        print('TMDB page not Found.')
+        return None
+
+    return returnDictionary
+
+
+
+print(getUpcomingMovies())
